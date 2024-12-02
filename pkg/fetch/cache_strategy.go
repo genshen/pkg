1b5ce5b0f869c319f30b55959ec249b6620e713a
genshen <genshenchu@gmail.com>
2024-12-02 09:09:02 +0800

    fix(fetch): fix incorrect branch and return value in cache determining algorithm
    Note: The cache strategy is: if both global cache and vendor src does not exist,
    pkg will download from remote; if only global cache exists, pkg will copy package
    from global cache to vendor src; if only vendor src exists, pkg will use vendor
    src package.
    
    This bug is involved by e57c5656
    see also PR #24

--- pkg/fetch/cache_strategy.go
+++ pkg/fetch/cache_strategy.go
@@ -1,7 +1,6 @@
 package fetch
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/genshen/pkg"
@@ -19,14 +18,14 @@ type CacheStrategy int
 
 // determinePackageCacheStrategy determines package cache strategy via package filesystem path.
 func determinePackageCacheStrategy(packageMeta pkg.PackageMeta, projectRoot string) (error, CacheStrategy) {
-	pkgVendorPath := packageMeta.HomeCacheSrcPath() // package global cache dir
-	pkgCachePath := packageMeta.VendorSrcPath(projectRoot)
+	pkgCachePath := packageMeta.HomeCacheSrcPath() // package global cache dir
+	pkgVendorPath := packageMeta.VendorSrcPath(projectRoot)
 
 	// copy only when global cache exists
 	if _, err := os.Stat(pkgVendorPath); os.IsNotExist(err) { // vendor src does not exist.
 		if _, err := os.Stat(pkgCachePath); err != nil { // global cache does not exist or other error.
 			if os.IsNotExist(err) {
-				return fmt.Errorf("cache and vendor path of package `%s` does not exists", packageMeta.PackageName), CacheStrategyDefault
+				return nil, CacheStrategyDownloadFromRemote
 			}
 			return err, CacheStrategySkip
 		} else { // vendor src does not exist, but global cache exist.
